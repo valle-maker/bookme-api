b7a55c9f2e10fc2dfb5780eccbb472b7afa4998e
valle-maker <vallevalenciadiegoalejandrogmail@.com>
2026-05-06 17:06:02 -0600

    chore: initial project setup with spring Initializr

---
+++ src/main/java/com/bookme/bookme_api/BookmeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookme.bookme_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookme/bookme_api/BookmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookme.bookme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
